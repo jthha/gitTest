34384f52be2e0119d552330b1c752df08d4d88d4
jthha <0wjsxoghks@gmail.com>
2015-03-23 12:24:37 +0900

    2015.3.23

---
+++ Assets/Script/InitScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitScript : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Rotate (Vector3.right * Time.deltaTime*100f);
+	}
+}
